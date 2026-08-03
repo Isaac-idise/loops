56b16d9f0b772a720b830303f8a14d58eadc5d2e
Isaac-idise <isaacidise15@gmail.com>
2026-08-03 22:37:39 +0100

    Add files via upload

---
+++ LargestNum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class LargestNum {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int largest = 0;
+
+      for (int counter = 1; counter <= 10; counter++) {
+          System.out.print("Enter number: ");
+          int number = scanner.nextInt();
+
+          if( number >  largest) {
+              largest = number;
+          }
+      }
+        System.out.println("The largest number is: "+ largest);
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+Start
+
+set largest to 0
+
+Prompt user to enter desired number 
+then repeat 10 times
+
+compare number to largest
+
+if number > largest
+
+set largest as number
+
+Stop
+
+print largest
+
+Stop
+

eed6d6b94a21dd9b70854e728eb7311e12afdaaf
Isaaaac <isaacidise15@gmail.com>
2026-08-04 11:23:55 +0100

    Add loop for multiple earnings input
    Modified the program to allow up to three earnings inputs in a loop.

--- TotalTax.java
+++ TotalTax.java
@@ -4,24 +4,25 @@ public class TotalTax {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter earnings: ");
-        double earnings = scanner.nextDouble();
+        for(int i = 0; i <= 3; i++) {
 
-        if(earnings == 0 || earnings < 0) {
-            System.out.println("Invalid amount");
+            System.out.print("Enter earnings: ");
+            double earnings = scanner.nextDouble();
+
+            if(earnings == 0 || earnings < 0) {
+                System.out.println("Invalid amount");
+            }
+            else if(earnings > 30000) {
+                double rate = 0.2;
+                double Total_tax = earnings * rate;
+                System.out.println("The Total tax is: " + Total_tax);
+            }
+            else {
+                double rate = 0.15;
+                double Total_tax = earnings * rate;
+                System.out.println("The Total tax is: " + Total_tax);
+                    }
+                }
+            }
         }
-        else if(earnings > 30000) {
-            double rate = 0.2;
-            double Total_tax = earnings * rate;
-            System.out.println("The Total tax is: " + Total_tax);
-        }
-        else {
-            double rate = 0.15;
-            double Total_tax = earnings * rate;
-            System.out.println("The Total tax is: " + Total_tax);
-        }
 
-
-
-    }
-}
